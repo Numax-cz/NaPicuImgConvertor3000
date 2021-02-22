777a95e410afda4d8a4af540cc6d927797d42a16
Marcel Mikolas <marcel.mikolas@gmail.com>
2021-02-22 19:55:14 +0100

    Program

---
+++ konvertor3000.py
@@ -0,0 +1,6 @@
+from PIL import Image  
+import glob
+for file in glob.glob("*.png"):
+    img = Image.open(file)
+    rgb_im = img.convert('RGBA')
+    rgb_im.save(file.replace("png", "webp"), quality=100)
